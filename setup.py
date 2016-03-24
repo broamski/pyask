b9a32a6c0edd18b79fc30c8d396d7158cfda641e
broamski <brian.nuszkowski@me.com>
2016-03-24 11:57:52 -0400

    Adding setup script

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='pyask',
+    version='0.0.1',
+    description='Easily build responses for the '
+                'Alexa Skills Kit (ASK) Interface',
+    author='Brian Nuszkowski',
+    author_email='nuszkowski@protonmail.com',
+    packages=find_packages(),
+    url='https://github.com/broamski/pyask'
+)
